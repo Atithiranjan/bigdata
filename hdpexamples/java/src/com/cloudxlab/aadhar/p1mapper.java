95ea6f55aa51296c7aaf2c2dffa17f05a0285a9f
rj3hadoop <rajbharath.cse@gmail.com>
2017-07-10 18:44:17 +0530

    Update p1mapper.java

--- hdpexamples/java/src/com/cloudxlab/aadhar/p1mapper.java
+++ hdpexamples/java/src/com/cloudxlab/aadhar/p1mapper.java
@@ -20,22 +20,22 @@ public void map(Object key, Text value, Context context) throws IOException, Int
         rej = Integer.parseInt(words[3]);
         if(i==0)
          {
-         String sa = "A1" + words[0];
+         String sa = "SA" + words[0];
          context.write(new Text(sa),new LongWritable(apr));
          }
          else if(i==1)
          {
-         String sr = "A2" + words[0];
+         String sr = "SR" + words[0];
           context.write(new Text(sr),new LongWritable(rej));
          }
          else if(i==2)
          {
-         String ca = "A3" + words[1];
+         String ca = "CA" + words[1];
           context.write(new Text(ca),new LongWritable(apr));
          }
          else 
          {
-         String cr = "A4" + words[1];
+         String cr = "CR" + words[1];
           context.write(new Text(cr),new LongWritable(rej));
          }
       }   
