31ebef1ff9fe0e872d69acd39af478e56dd53ad4
rj3hadoop <rajbharath.cse@gmail.com>
2017-07-10 19:04:54 +0530

    Create p1reducer.java

---
+++ hdpexamples/java/src/com/cloudxlab/aadhar/p1reducer.java
@@ -0,0 +1,24 @@
+package com.aadhar.p1reducer;
+import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class p1reducer extends Reducer<Text,LongWritable,Text,LongWritable>
+{
+void reduce(Text key,Iterable<LongWritable> values,Context context) 
+throws IOException, InterruptedException
+{
+ String flag = key.substring(2,key.length());
+ long sum =0;
+for(LongWritable iw:values) 
+{
+ sum = sum + iw
+}
+context.write(new Text(flag),new LongWritable(sum))
+}
+}
+
