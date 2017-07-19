55297e8e5a4b2032247b08f3fb0b151a6787e3bd
rj3hadoop <rajbharath.cse@gmail.com>
2017-07-19 13:32:18 +0530

    Create sujireducer.java

---
+++ hdpexamples/java/src/com/cloudxlab/sujimr/sujireducer.java
@@ -0,0 +1,26 @@
+package com.cloudxlab.sujimr;
+import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class sujireducer extends Reducer<Text, LongWritable, Text, LongWritable>
+throws IOException, InterruptedException
+{
+public void reduce(Text key, Iterable <LongWritable> values, Context context)
+{
+Long sum = 0;
+for(LongWritable iw:values)
+{
+ sum = sum + iw.get();
+}
+
+context.write(key, new LongWritable(sum));
+
+}
+
+
+}
