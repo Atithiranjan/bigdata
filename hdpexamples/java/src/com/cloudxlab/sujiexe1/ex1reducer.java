ec2e7a7188577ac1d2d047e4ad938f3a34778875
rj3hadoop <rajbharath.cse@gmail.com>
2017-07-20 19:47:07 +0530

    Create ex1reducer.java

---
+++ hdpexamples/java/src/com/cloudxlab/sujiexe1/ex1reducer.java
@@ -0,0 +1,25 @@
+package com.cloudxlab.aadhar;
+import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class ex1reducer extends Reducer<Text,LongWritable,Text,LongWritable>
+{
+public void reduce(Text key,Iterable<LongWritable> values,Context context) 
+throws IOException, InterruptedException
+{
+ String fkey = key.toString();
+ String flag = fkey.substring(2,fkey.length());
+ long sum =0;
+for(LongWritable iw:values) 
+{
+ sum += iw.get();
+}
+context.write(new Text(flag),new LongWritable(sum));
+}
+}
+
