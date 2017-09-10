cb51fb0537166a81f3f0d7faa8c803d284b918b1
rj3hadoop <rajbharath.cse@gmail.com>
2017-09-11 01:15:37 +0530

    Update pdfinput.java

--- hdpexamples/java/src/com/cloudxlab/sensex/pdfinput.java
+++ hdpexamples/java/src/com/cloudxlab/sensex/pdfinput.java
@@ -1,1 +1,26 @@
-
+package com.cloudxlab.sensex;
+import java.io.IOException;
+ 
+import org.apache.hadoop.io.IntWritable;
+ 
+import org.apache.hadoop.mapreduce.InputSplit;
+ 
+import org.apache.hadoop.mapreduce.RecordReader;
+ 
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+ 
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+ 
+public class pdfinput extends FileInputFormat<Key_value,IntWritable> {
+ 
+@Override
+ 
+public RecordReader<Key_value,IntWritable> createRecordReader(InputSplit arg0,
+ 
+TaskAttemptContext arg1) throws IOException, InterruptedException {
+ 
+return new pdfRecordReader();
+ 
+}
+ 
+}
